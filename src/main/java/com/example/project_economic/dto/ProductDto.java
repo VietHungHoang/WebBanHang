1c5b5b3e789410e9acf3aa5536c9c16ec5fbffa2
VietHungHoang <viethunghoang1508@gmail.com>
2023-11-30 17:35:42 +0700

    05:32 PM - 11:30:2023 - Final

---
+++ src/main/java/com/example/project_economic/dto/ProductDto.java
@@ -0,0 +1,25 @@
+package com.example.project_economic.dto;
+
+import com.example.project_economic.entity.CategoryEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Long costPrice;
+    private Long salePrice;
+    private Integer currentQuantity;
+    private Integer likes;
+    private String image;
+    private Long  categoryId;
+    private Boolean is_deteted;
+    private Boolean is_actived;
+    private Long sellerId;
+}
